cf6f9972b1cbe8a91d053a8736666fb869482725
GuyR <guyr@wix.com>
2015-01-07 00:35:24 +0200

    question 6 in progress

--- src/hello.js
+++ src/hello.js
@@ -2,15 +2,25 @@ define(['react', 'lodash', './hello.rt', 'deepLinkingMixin'], function (React, _
     'use strict';
 
 
+
     return React.createClass({
         displayName: 'Hello',
         mixins: [deepLinkingMixin],
         getInitialState: function(){
+            this.geoData = {
+                israel: ['Tel-Aviv', 'Haifa' ,'Jerusalem'],
+                usa: ['New York', 'Los Angeles', 'New Haven', 'Boston'],
+                spain: ['Barcelona', 'Girona', 'Madrid'],
+                countries: ['israel', 'usa', 'spain']
+            }
             return {
                 location : {
                     lat: 0,
                     lon: 0
-                }
+                },
+                country: '',
+                city: '',
+                ready: false
             }
         },
         render: function(){
