465a256ec1180f4ace461db95a513195ad2eae55
GuyR <guyr@wix.com>
2015-01-06 09:25:57 +0200

    question 4 option 1

--- src/hello.js
+++ src/hello.js
@@ -1,13 +1,43 @@
 define(['react', 'lodash', './hello.rt', 'utils'], function (React, _, template, /** utils */utils) {
     'use strict';
 
+
     return React.createClass({
         displayName: 'Hello',
         getInitialState: function(){
             return {
-                name : utils.getUrlParam('name')
+                location : {
+                    lat: 0,
+                    long: 0
+                }
             }
         },
-        render: template
+        handleLatChange: function(lat){
+            this.setState({
+                location : {
+                    lat: lat,
+                    long: this.state.location.long
+                }
+            })
+        },
+        handleLongChange: function(long){
+            this.setState({
+                location : {
+                    lat: this.state.location.lat,
+                    long: long
+                }
+            })
+        },
+        render: function(){
+            this.latValueLink = {
+                value: this.state.location.lat,
+                requestChange: this.handleLatChange
+            }
+            this.longValueLink = {
+                value: this.state.location.long,
+                requestChange: this.handleLongChange
+            }
+            return template.apply(this);
+        }
     });
 });
