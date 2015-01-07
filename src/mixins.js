4098e274a67d732269d42e91f1a6dee375497634
GuyR <guyr@wix.com>
2015-01-07 22:06:06 +0200

    Question 9 solution

--- src/mixins.js
+++ src/mixins.js
@@ -66,8 +66,49 @@ define(['lodash', 'utils'], function (_, /** utils */ utils) {
         }
     };
 
+    function getObjectProperty(stateObj, pathArray){
+        var propertyParent;
+        if (pathArray.length === 0){
+            propertyParent = stateObj;
+        } else {
+            propertyParent = _.reduce(pathArray, function(accumulator, value){
+                return accumulator[value];
+            }, stateObj, this);
+        }
+        return propertyParent;
+    }
+
+    function getInPath(obj, path){
+        if (path.length === 1){
+            return obj[path[0]];
+        }
+
+        return getInPath(obj[path[0]], path.slice(1));
+    }
+
+    var deepLinkMixin = {
+        deepLinkState: function(statePath){
+
+            return {
+                value: getInPath(this.state, statePath.split('.')),
+                requestChange: function(value){
+                    var pathArray = statePath.split('.');
+                    var propertyName = pathArray[pathArray.length - 1];
+                    pathArray = pathArray.slice(0, pathArray.length - 1);
+                    var newState = _.cloneDeep(this.state);
+
+                    var clonedPropertyParent = getObjectProperty(newState, pathArray);
+                    clonedPropertyParent[propertyName] = value;
+                    this.setState(newState);
+                }.bind(this)
+            }
+        }
+    };
+
+    /** @class mixins*/
     return {
         translationMixin: translationMixin,
-        inputMixin: inputMixin
+        inputMixin: inputMixin,
+        deepLinkMixin: deepLinkMixin
     };
 });
--- src/sliderProblem.js
+++ src/sliderProblem.js
@@ -1,7 +1,15 @@
-define(['react', 'lodash', './sliderProblem.rt'], function (React, _, template) {
+define(['react', 'lodash', 'mixins', './sliderProblem.rt'], function (React, _, /** mixins */ mixins, template) {
     'use strict';
     return React.createClass({
         displayName: 'Hello',
+        getInitialState: function(){
+          return {
+              panelHeight: 300,
+              innerBoxPosition: 50,
+              innerBoxHeight: 100
+          }
+        },
+        mixins: [mixins.deepLinkMixin],
         render: template
     });
 });
