48b29d3256676571bcaf82aa557fb53105c8fb24
GuyR <guyr@wix.com>
2015-01-08 11:47:59 +0200

    panel templates

--- src/templateWrapper.js
+++ src/templateWrapper.js
@@ -1,9 +1,18 @@
-define(['react', 'lodash', 'mixins', 'templateWrapper.rt'], function (React, _, /** mixins */ mixins, template) {
+define(['react', 'lodash', 'mixins', 'templateWrapper.rt', 'settingsPanel.rt'], function (React, _, /** mixins */ mixins, template, contentTemplate) {
     'use strict';
 
     return React.createClass({
         displayName: 'templateWrapper',
         mixins: [mixins.deepLinkMixin],
-        render: template
+        render: function(){
+            var fullTemplate = template.apply(this);
+            var content = contentTemplate.apply(this);
+            var templateChildren = fullTemplate.props.children;
+            var contentIndex = _.findIndex(templateChildren, function(child){
+                return child.ref === 'content';
+            });
+            templateChildren.splice(contentIndex, 1, content);
+            return fullTemplate;
+        }
     });
 });
